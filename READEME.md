5a2f001eaf6d65eb16534972fb7574d5180688dd
JiyoungCha <isasegun@gmail.com>
2025-11-12 18:05:47 +0900

    251112 DB연동

---
+++ READEME.md
@@ -0,0 +1,23 @@
+# 유효성 체크를 위한 준비   
+## 설치
+```
+npm install express-validator
+```
+##  유효성 체크 처리 디렉토리 구조
+```
+middlewares/
+└─ validations/
+  ├─ fields/     # 각 요소들의 유효성 체크 처리 로직이 있는 디렉토리
+  ├─ validators/ # 필요한 field를 모아서 한기능에서 사용하는 데이터들을 검증하는 로직이 들어가 있는 디렉토리
+  └─ validationHandler.js # 유효성 검사 통과 여부에 따른 공토 처리 미들웨어
+```
+
+# DB연동
+## 설치
+```
+npm i mysql2 sequelize sequelize-cli
+```
+# 날짜관련 js 라이브러리
+```
+npm i dayjs
+```
--- routes/users.router.js
+++ routes/users.router.js
@@ -1,10 +1,40 @@
 import express from 'express';
-import { eduUsersTest } from '../app/middlewares/edu/edu.middleware.js';
+// import pool from '../db/my-db.js';
+import db from '../app/models/index.js'
+const { sequelize, Employee } = db;
 
 const usersRouter = express.Router();
 
 usersRouter.get('/', (request, response, next) => {
-  response.status(200).send('유저 정보 조회 완료');
+  response.status(200).send('전체 유저 정보 조회 완료');
+});
+
+usersRouter.get('/:id', async (request, response, next) => {
+  try {
+    const id = parseInt(request.params.id);
+    // -------------------
+    // Sequelize로 DB연동
+    // -------------------
+    const result = await Employee.findByPk(id);
+    return response.status(200).send(result);
+    
+    // -------------------
+    // mysql2로 DB연동
+    // -------------------
+    // // 쿼리 작성
+    // const sql = `
+    //   SELECT *
+    //   FROM employees
+    //   WHERE
+    //     emp_id = ?
+    // `;
+
+    // const [result] = await pool.query(sql);
+
+    // return response.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 usersRouter.put('/:id', (request, response, next) => {
